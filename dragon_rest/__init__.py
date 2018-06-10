4337221fe13a386da4c3ba3ec96afffd10e07a09
Brenden Matthews <brenden@diddyinc.com>
2018-06-10 15:57:49 -0400

    Add build stuff.

---
+++ dragon_rest/__init__.py
@@ -0,0 +1,1 @@
+name = "dragon_rest"
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dragon_rest",
+    version="0.0.1",
+    author="Brenden Matthews",
+    author_email="brenden@diddyinc.com",
+    description="Python wrapper for DragonMint T1 REST API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/brndnmtthws/dragon-rest",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache License",
+        "Operating System :: OS Independent",
+    ),
+)
