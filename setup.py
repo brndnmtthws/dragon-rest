04700fbf576d65bd8feaee6b68eb58f6a9698415
Brenden Matthews <brenden@diddyinc.com>
2018-06-16 18:14:48 -0400

    Bump version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dragon-rest",
-    version="0.0.8",
+    version="0.0.9",
     author="Brenden Matthews",
     author_email="brenden@diddyinc.com",
     description="Python wrapper for DragonMint T1 REST API",
